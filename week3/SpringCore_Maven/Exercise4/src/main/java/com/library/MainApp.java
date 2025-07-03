f23d452918695703c8b80abd2cc3986d34f92aa7
peramruthvika <peramruthvika@gmail.com>
2025-07-03 15:23:03 +0530

    Added Mandatory Exercises

---
+++ week3/SpringCore_Maven/Exercise4/src/main/java/com/library/MainApp.java
@@ -0,0 +1,12 @@
+package com.library;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        System.out.println("Spring Context Loaded Successfully!");
+        ((ClassPathXmlApplicationContext) context).close();
+    }
+}
