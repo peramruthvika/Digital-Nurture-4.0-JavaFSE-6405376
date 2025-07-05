c61ebd0a75ec932159e97821e91eed3746064b5c
peramruthvika <peramruthvika@gmail.com>
2025-07-05 11:24:10 +0530

    Deleted SpringData_JPA folder and related files

--- week3/SpringData_JPA/spring-data-jpa-quick-example/src/main/java/com/ruthvika/country/model/repository/CountryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ruthvika.spring_data_jpa_quick_example.repository;
-
-import com.ruthvika.spring_data_jpa_quick_example.model.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, String> {}
