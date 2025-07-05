14489bb5441b7a3d152cb7e6a9cafd25a1afdfa9
peramruthvika <peramruthvika@gmail.com>
2025-07-05 13:10:21 +0530

    Added Spring  Exercises

---
+++ week3/SpringData_JPA/Quick_Example/src/main/java/com.cognizant.orm_learn/service/CountryService.java
@@ -0,0 +1,28 @@
+package com.cognizant.orm_learn.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.repository.CountryRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+
+    @Transactional
+    public void addCountry(Country country) {
+        countryRepository.save(country);
+    }
+}
