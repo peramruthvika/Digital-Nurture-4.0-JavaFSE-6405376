1727517fa98fe27c2b345eed4f24787afcfee362
peramruthvika <peramruthvika@gmail.com>
2025-07-24 11:37:56 +0530

    Re-added Week 6 folders

--- week6/cohortstracker/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders cohort tracker title', () => {
-  render(<App />);
-  const heading = screen.getByText(/Cohorts Details/i);
-  expect(heading).toBeInTheDocument();
-});
